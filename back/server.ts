1c46e777912af93035a470faa81f8ae1802c2226
Rontard Noah <rontard.noah@gmail.com>
2025-11-12 17:56:58 +0100

    front request to api + server

--- back/server.ts
+++ back/server.ts
@@ -1,14 +1,27 @@
 import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { join } from "path";
+import { request } from "http";
 
 const fastify = Fastify({
   logger: true,
 });
 
+fastify.register(fastifyStatic, {
+  root: join(process.cwd(), "front"),
+  prefix: "/",
+});
+
 fastify.get("/", async (request, reply) => {
-  return  `
-    <h1>Bienvenue 👋</h1>
-    <p>C'est la page d'accueil.</p>
-  `;
+  return reply.sendFile("index.html");
+});
+
+fastify.post("/api/register", async (request, reply) => {
+  const { username, email, password } = request.body as any;
+
+  console.log("Register data:", username, email, password);
+
+  return { message: `Utilisateur ${username} enregistré avec succès !` };
 });
 
 const start = async () => {
