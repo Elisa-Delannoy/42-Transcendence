60f9dbc238d2da3a5ec867dce9b37284a0514fc3
edelannoperso <elisadelannoy10@gmail.com>
2026-01-09 14:07:41 +0100

    console.log debug websocket

--- front/public/app.js
+++ front/public/app.js
@@ -4062,7 +4062,17 @@ function initSocket() {
     transports: ["websocket"],
     query: { token }
   });
+  globalSocket.on("connect_error", (err) => {
+    console.log("CONNECT ERROR:", err.message);
+  });
+  console.log("Socket attempting connection...");
+  globalSocket.on("connect", () => console.log("CONNECTED!"));
+  globalSocket.on("connect_error", (err) => console.log("CONNECT ERROR:", err));
+  globalSocket.on("error", (err) => console.log("ERROR:", err));
   console.log((/* @__PURE__ */ new Date()).toISOString(), "globalsocket s ocket=", globalSocket);
+  setTimeout(() => {
+    console.log("Final socket state:", globalSocket);
+  }, 500);
 }
 var globalSocket;
 var init_socket3 = __esm({
--- front/src/socket/socket.ts
+++ front/src/socket/socket.ts
@@ -11,5 +11,19 @@ export function initSocket() {
         transports: ["websocket"],
         query: { token }
     });
+    globalSocket.on("connect_error", (err) => {
+    console.log("CONNECT ERROR:", err.message);
+});
+
+    console.log("Socket attempting connection...");
+    globalSocket.on("connect", () => console.log("CONNECTED!"));
+    globalSocket.on("connect_error", (err) => console.log("CONNECT ERROR:", err));
+    globalSocket.on("error", (err) => console.log("ERROR:", err));
+
+
     console.log(new Date().toISOString(), "globalsocket s ocket=", globalSocket);
+    setTimeout(() => {
+    console.log("Final socket state:", globalSocket);
+}, 500);
+
 }
